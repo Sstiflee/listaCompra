3c88b5cfe4f4b8ade3613c86c6c8c80e17eac3ad
adrianbelartezapata <adrian.belartezapata@cieep.com>
2023-11-03 09:50:31 +0100

    Initial commit

---
+++ app/src/main/java/adrian/belarte/listacompra/MainActivity.java
@@ -0,0 +1,40 @@
+package adrian.belarte.listacompra;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+
+
+import adrian.belarte.listacompra.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
